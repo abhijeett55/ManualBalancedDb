5067eb956b342fc82df5ddb4fd5db7373337fa48
abhijeett55 <100180572+abhijeett55@users.noreply.github.com>
2025-12-08 18:08:06 +0530

    Updates Create

--- src/sql/CreateStatement.hpp
+++ src/sql/CreateStatement.hpp
@@ -1,12 +1,14 @@
 #ifndef CREATESTATEMENT_HPP
 #define CREATESTATEMENT_HPP
 
+#include "SQLStatement.hpp"
+
 
 namespace sql {
     struct SelectStatement;
 
+    // Definition of Table column
     struct ColumnDefinition {
-
         enum DataType {
             UNKNOWN,
             TEXT,
@@ -23,12 +25,26 @@ namespace sql {
 
     };
 
-    struct CreateStatement : {
+    // Represent SQL Create Statement
+    /** EXAMPLE- "CREATE TABLE students (name TEXT, student_number INTEGER, city TEXT, grade DOUBLE)" **/ 
+    struct CreateStatement :  SQLStatement {
         enum CreateType {
-            kTable;
-            
+            kTable,
+            kTableFromTbl,
+            kView,
         };
 
+        CreateStatement(CreateType type);
+        virtual ~CreateStatement();
+
+        CreateType type;
+        bool ifNotExists;
+        char* filePath;
+        char* tableName;
+
+        std::vector<ColumnDefinition*>* columns;
+        std::vector<char*>* viewColumns;
+        SelectStatement* select;
     };
 }
 
