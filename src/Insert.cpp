0d62157c7448f070b15d7a569785bfa978e5d3f0
abhijeett55 <100180572+abhijeett55@users.noreply.github.com>
2026-02-06 20:09:40 +0530

    update sql

--- src/Insert.cpp
+++ src/Insert.cpp
@@ -20,4 +20,27 @@ void insert_command(char tname[], void *data[], int total) {
     table *temp;
     int ret;
     BPTree obj(tname);
+    FILE *fp = open_file(tname, const_cast<char*>("r"));
+    temp = (table*)malloc(sizeof(table));
+    fread(temp, sizeof(table), 1, fp);
+}
+
+
+
+void insert() {
+    char *tab;
+    tab = (char*)malloc(sizeof(char)*MAX_PATH+1);
+    std::cout<< "Enter table Name: ";
+    std::cin>>tab;
+    int check = search_table(tab);
+    if(check == 0) {
+        printf("\n Table \" %s \" don't exist in database\n", tab);
+        return;
+    } else {
+        std::cout<<"\nTable exists, enter data\n\n";
+
+        table input1;
+        int count;
+        File *fp = open_file(tab, const_cast<char*>("r"));
+    }
 }
